9df7aa0ba06c83be6b98b43915889772cfe8b003
Eric Wong <ericwong12@live.com>
2019-11-27 15:05:42 -0700

    index.html just a base htmll page index.js has the psudo code and the function generatepassword plus variables, lowercase, uppercase, passwordnumber, and password special

---
+++ index.js
@@ -0,0 +1,16 @@
+//this is the function to generate the password
+function generatePassword(){
+
+//Minimum requirement needed for the password length
+
+//The variables needed to generate the random charactors
+var lowerCaseLetter = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCaseLetter = lowerCaseLetter.toUpperCase();
+var passwordNumber = ["0123456789"];
+var passwordSpecial =["~!@#$%^&*()_+"];
+
+//need an alert that would prompt the user if they(continuation to next line)
+//would like to have certain charactor. Minimum of one charactor must be chosen.
+
+//bonus copy to a clipboard
+}
