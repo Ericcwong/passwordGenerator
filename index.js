4d37f927bcfae06e792eeb600bc83498cc5d3009
Eric Wong <ericwong12@live.com>
2019-11-27 16:31:25 -0700

    index.js added a while loop that continues to loop if user doesnt enter a number then an if statement if user enters a number over 8 or 128

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 //this is the function to generate the password
 function generatePassword(){
 
-//Minimum requirement needed for the password length
+    //The variables needed to generate the random charactors
+    var passwordLength = null;
+    var lowerCaseLetter = ['abcdefghijklmnopqrstuvwxyz'];
+    var upperCaseLetter = lowerCaseLetter.toUpperCase();
+    var passwordNumber = ['0123456789'];
+    var passwordSpecial = ['~!@#$%^&*()_+'];
 
-//The variables needed to generate the random charactors
-var lowerCaseLetter = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCaseLetter = lowerCaseLetter.toUpperCase();
-var passwordNumber = ["0123456789"];
-var passwordSpecial =["~!@#$%^&*()_+"];
+    //Minimum requirement needed for the password length
+    while (passwordLength == null){
+    var passwordLength = prompt("Enter desired number of charactor you want your password to be.");
+    passwordLength ==Number(passwordLength);
+    if (passwordLength < 8 || passwordLenth > 128){
+        alert("Please enter a password that is between 8 to 128 charactors.");
+        }
+    }
+}
+
 
 //need an alert that would prompt the user if they(continuation to next line)
 //would like to have certain charactor. Minimum of one charactor must be chosen.
 
 //bonus copy to a clipboard
-}
