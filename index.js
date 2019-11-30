f2c3e8130beac351f6f62fa0f117d699dcb3e557
Eric Wong <ericwong12@live.com>
2019-11-29 17:06:08 -0700

    index.html: added link to style.css and button id changed to createPassword index.js: added two while loops, first while loop prompts user to enter a number from 8-128 characters if the number of character is over the loop repeats the second while loop is to check if users have chosen two choices minimum style.css has only test styles

--- index.js
+++ index.js
@@ -2,20 +2,30 @@
 function generatePassword(){
 
     //The variables needed to generate the random charactors
-    var passwordLength = null;
-    var lowerCaseLetter = ['abcdefghijklmnopqrstuvwxyz'];
+    var userPassword = 0
+    var lowerCaseLetter = 'abcdefghijklmnopqrstuvwxyz';
     var upperCaseLetter = lowerCaseLetter.toUpperCase();
-    var passwordNumber = ['0123456789'];
-    var passwordSpecial = ['~!@#$%^&*()_+'];
-
-    //Minimum requirement needed for the password length
+    var passwordNumber = '0123456789';
+    var passwordSpecial = '~!@#$%^&*()_+';
+    var passwordLength = null;
+    //Minimum requirement character needed for the password length
     while (passwordLength == null){
-    var passwordLength = prompt("Enter desired number of charactor you want your password to be.");
-    passwordLength ==Number(passwordLength);
-    if (passwordLength < 8 || passwordLenth > 128){
-        alert("Please enter a password that is between 8 to 128 charactors.");
+        var passwordLength = prompt("Enter an desired number of charactor you want your password to be.","8 to 128 characters");
+        passwordLength == Number(passwordLength);
+        if (passwordLength < 8 || passwordLength > 128){
+            alert("Please enter a password that is between 8 to 128 charactors.");
+            passwordLength = null;
         }
     }
+    // The option for the user to choose if they would want lower case letters 
+    while ( = 0){
+        var lowerCaseLetter = confirm("Do you want lower case letters in your password?");
+        if(lowerCaseLetter == true){
+            userPassword = userPassword + lowerCaseLetter;
+        }
+        
+    }
+
 }
 
 
