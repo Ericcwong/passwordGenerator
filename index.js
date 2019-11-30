ebd76aee4de46bda1a3015104cdcceebc4722fb9
Eric Wong <ericwong12@live.com>
2019-11-29 19:51:24 -0700

    index.js: added confirms so the user can choose if they want lowercase, uppercase, numbers, and or special characters. not fully working yet

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 function generatePassword(){
 
     //The variables needed to generate the random charactors
-    var userPassword = 0
+    var userPassword = ''
+    var userPasswordChoices = 0;
     var lowerCaseLetter = 'abcdefghijklmnopqrstuvwxyz';
     var upperCaseLetter = lowerCaseLetter.toUpperCase();
     var passwordNumber = '0123456789';
@@ -18,18 +19,34 @@ function generatePassword(){
         }
     }
     // The option for the user to choose if they would want lower case letters 
-    while ( = 0){
-        var lowerCaseLetter = confirm("Do you want lower case letters in your password?");
-        if(lowerCaseLetter == true){
-            userPassword = userPassword + lowerCaseLetter;
-        }
+    while (userPasswordChoices == 0){
+    var lowerCase = confirm("Do you want lower case letters in your password?");
+    if(lowerCase == true){
+        userPassword = userPassword + lowerCaseLetter;
+        userPasswordChoices++;
         
     }
-
+    var upperCase = confirm("Do you want upper case letters in your password?");
+    if(upperCase == true){
+        userPassword = userPassword + upperCaseLetter;
+        userPasswordChoices++;
+        
+    }
+    var numbers = confirm("Do you want numbers in your password?");
+    if(numbers == true){
+        userPassword = userPassword + passwordNumber;
+        userPasswordChoices++;
+        
+    }
+    var special = confirm("Do you want special characters letters in your password?");
+    if(special == true){
+        userPassword = userPassword + passwordSpecial;
+        userPasswordChoices++;
+    }else{ alert("Please pick at least two")}
+    console.log(userPassword);
 }
 
-
+}
 //need an alert that would prompt the user if they(continuation to next line)
 //would like to have certain charactor. Minimum of one charactor must be chosen.
-
-//bonus copy to a clipboard
+//bonus copy to clipboard
