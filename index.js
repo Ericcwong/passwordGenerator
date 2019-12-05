629c985547e02ee6d08052041d5de3ca72dfa2ad
Eric Wong <ericwong12@live.com>
2019-12-05 13:27:47 -0700

    index.js code is working, changed the whole function to have it. When the user clicks on Generate Password it would execute out the generate on click
    BUG: the old password is still there when user chooses to click generate password again.

--- index.js
+++ index.js
@@ -7,39 +7,44 @@
     var userPassword = ""; //This would be the end result of the password
     var passwordChoices = ""; //This is what the user has chosen and combines it into one variable
     
-//The variables that calls the checkboxes
-    var passwordLength = document.getElementById("inputPassLength");//Where the user inputs in the desired length
-
-    var lowerCheckBox = document.getElementById("lowerCaseCheck");
-    var upperCheckBox = document.getElementById("upperCaseCheck");
-    var numberCheckBox= document.getElementById("numberCheck");
-    var specialCheckBox = document.getElementById("specialCheck");
-
     var printPassword = document.getElementById("password");//
     var generatePassword = document.getElementById("generateBtn");
-//When the variables are called function is ran to add what they checked to the variable passwordChoices
-    lowerCheckBox.addEventListener("change",function(event){
-        if(event.target.checked){
-        passwordChoices = passwordChoices + lowerCaseLetter;
+   
+//When generatePassword is clicked it would have to run through the variables to check if the checked boxes are checked or not.
+    generatePassword.addEventListener("click", function(){
+        
+        var passwordLength = document.getElementById("inputPassLength").value;//Where the user inputs in the desired length
+        var lowerCheckBox = document.getElementById("lowerCaseCheck").checked;
+        var upperCheckBox = document.getElementById("upperCaseCheck").checked;
+        var numberCheckBox= document.getElementById("numberCheck").checked;
+        var specialCheckBox = document.getElementById("specialCheck").checked;
+        if(passwordLength < 8 || passwordLength > 128){
+            alert("Please choose a length of 8 to 128.");
+            
+        }console.log(passwordLength);
+
+        if (lowerCheckBox === true){
+            passwordChoices = passwordChoices + lowerCaseLetter;
         }
-    });
-    upperCheckBox.addEventListener("change",function(event){
-        if(event.target.checked){
-        passwordChoices = passwordChoices + upperCaseLetter;
+        if (upperCheckBox === true){
+            passwordChoices = passwordChoices + upperCaseLetter;
         }
-    });
-    numberCheckBox.addEventListener("change",function(event){
-        if(event.target.checked){
-        passwordChoices = passwordChoices + passwordNumber;
+        if (numberCheckBox === true){
+            passwordChoices = passwordChoices + passwordNumber;
         }
-    });
-    specialCheckBox.addEventListener("change",function(event){
-        if(event.target.checked){
-        passwordChoices = passwordChoices + passwordSpecial;
+        if (specialCheckBox === true){
+            passwordChoices = passwordChoices + passwordSpecial;
         }
+
+        for(i=0; i<passwordLength; i++){
+            userPassword += passwordChoices.charAt(Math.floor(Math.random() * passwordChoices.length));
+        }
+        
+
+        console.log(lowerCheckBox,upperCheckBox,numberCheckBox,specialCheckBox);
+        console.log(passwordChoices);
+        console.log(userPassword);
+        printPassword.textContent = userPassword;
     });
- 
-    generatePassword.addEventListener("click",function(){
-        const length = +passwordLength.value;
-        console.log(length);
-    });
+   
+
