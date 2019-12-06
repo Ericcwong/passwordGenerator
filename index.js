65a55945bde78418deb851a1e9d2b247cdebe0d4
Eric Wong <ericwong12@live.com>
2019-12-06 14:05:30 -0700

    fixed some bugs Bugs were when the user clicked generate password more than once the passwordchoices pool would not reset, that was fixed by adding passwordchoices = ""; to the top of the function to clear it added return to when the password is <8 or >128 added return to checked boxes are false

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //The variables needed to generate the random charactors
-    var lowerCaseLetter = "abcdefghijklmnopqrstuvwxyz";
+    const lowerCaseLetter = "abcdefghijklmnopqrstuvwxyz";
     var upperCaseLetter = lowerCaseLetter.toUpperCase();
     var passwordNumber = "0123456789";
     var passwordSpecial = "~!@#$%^&*()_+";
@@ -9,42 +9,66 @@
     
     var printPassword = document.getElementById("password");//
     var generatePassword = document.getElementById("generateBtn");
+    var clipBoardBtn = document.getElementById("copy");
+    var copyText = document.getElementById("password");
    
 //When generatePassword is clicked it would have to run through the variables to check if the checked boxes are checked or not.
     generatePassword.addEventListener("click", function(){
         var userPassword = "";
+        var passwordChoices = "";
+//This pulls in the ids from the dom
         var passwordLength = document.getElementById("inputPassLength").value;//Where the user inputs in the desired length
         var lowerCheckBox = document.getElementById("lowerCaseCheck").checked;
         var upperCheckBox = document.getElementById("upperCaseCheck").checked;
         var numberCheckBox= document.getElementById("numberCheck").checked;
         var specialCheckBox = document.getElementById("specialCheck").checked;
-        if(passwordLength < 8 || passwordLength > 128){
-            alert("Please choose a length of 8 to 128.");
+ //This stop the code when the user chooses a length outside of 8 to 128       
+            if(passwordLength < 8 || passwordLength > 128){
+                alert("Please choose a length of 8 to 128.");
+                return generatePassword
+            }
+//This will stop the code from running if the user has selected nothing for the password
+            if( lowerCheckBox === false, 
+                upperCheckBox === false,
+                numberCheckBox === false,
+                specialCheckBox === false
+                    ){alert("Please choose at least one selector.")
+                    return generatePassword}
+//Checks if the checkboxes are true
+            if (lowerCheckBox === true){
+                passwordChoices = passwordChoices + lowerCaseLetter;
+            }
+            if (upperCheckBox === true){
+                passwordChoices = passwordChoices + upperCaseLetter;
+            }
+            if (numberCheckBox === true){
+                passwordChoices = passwordChoices + passwordNumber;
+            }
+            if (specialCheckBox === true){
+                passwordChoices = passwordChoices + passwordSpecial;
+            }
+
+            for(i=0; i<passwordLength; i++){
+                userPassword += passwordChoices.charAt(Math.floor(Math.random() * passwordChoices.length));
+            }
             
-        }console.log(passwordLength);
 
-        if (lowerCheckBox === true){
-            passwordChoices = passwordChoices + lowerCaseLetter;
-        }
-        if (upperCheckBox === true){
-            passwordChoices = passwordChoices + upperCaseLetter;
-        }
-        if (numberCheckBox === true){
-            passwordChoices = passwordChoices + passwordNumber;
-        }
-        if (specialCheckBox === true){
-            passwordChoices = passwordChoices + passwordSpecial;
-        }
-
-        for(i=0; i<passwordLength; i++){
-            userPassword += passwordChoices.charAt(Math.floor(Math.random() * passwordChoices.length));
-        }
-        
-
-        console.log(lowerCheckBox,upperCheckBox,numberCheckBox,specialCheckBox);
-        console.log(passwordChoices);
-        console.log(userPassword);
-        printPassword.textContent = userPassword;
+            console.log(lowerCheckBox,upperCheckBox,numberCheckBox,specialCheckBox);
+            console.log(passwordChoices);
+            console.log(userPassword);
+            printPassword.textContent = userPassword;
+    });
+//copies the password to clipboard
+    clipBoardBtn.addEventListener("click", function(){
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+      
+        /* Alert the copied text */
+        alert("Password has been added to the clipboard! " + copyText.value);
+      
     });
    
 
